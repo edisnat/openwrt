671bf7600d0d3bb3c9d644ce1e274bf276c1708d
Kevin Darbyshire-Bryant <ldir@darbyshire-bryant.me.uk>
2020-01-09 13:52:53 +0000

    kernel: 4.19.95
    Signed-off-by: Kevin Darbyshire-Bryant <ldir@darbyshire-bryant.me.uk>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-4.14 = .162
-LINUX_VERSION-4.19 = .93
+LINUX_VERSION-4.19 = .95
 
 LINUX_KERNEL_HASH-4.14.162 = f65170224cd4359ce8b2793b492bd8127abdd0b91350484e001bce13f0c98b4b
-LINUX_KERNEL_HASH-4.19.93 = 2af5d23f82a8bf69a6e4d8407579293b7beeebd3054dc464d48bdb5d2d7f5834
+LINUX_KERNEL_HASH-4.19.95 = 4aedca32eec13ac5103fc23e141f9221dfe120d1c0651c0a2efe7de2182f4fb0
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
